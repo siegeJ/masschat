e07345e63bd9187ba6735c283b4e2a9aa736231e
siegeJ <cjs0186c@gmail.com>
2018-03-08 20:20:50 -0600

    Configuration/AppSettings

---
+++ masschat/Configuration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace masschat
+{
+    public class Configuration
+    {
+        private IConfiguration AppSettingsConfig { get; set; }
+
+        private static Configuration instance;
+
+        public string Server { get; }
+        public int Port { get; }
+        public string Nick { get; }
+        public string Password { get; }
+
+        public static Configuration Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Configuration();
+                }
+                return instance;
+            }
+        }
+
+
+        private Configuration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory() + "\\settings\\")
+                .AddJsonFile("appsettings.json");
+
+            AppSettingsConfig = builder.Build();
+
+            Server = AppSettingsConfig["server"];
+            Port = Int32.Parse(AppSettingsConfig["port"]);
+            Nick = AppSettingsConfig["nick"];
+            Password = AppSettingsConfig["password"];
+
+        }
+
+    }
+}
---
+++ masschat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace masschat
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"MASS CHAT");
+            Console.WriteLine($"Server: {Configuration.Instance.Server}:{Configuration.Instance.Port}");
+            Console.WriteLine($"Nick: {Configuration.Instance.Nick}");
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
